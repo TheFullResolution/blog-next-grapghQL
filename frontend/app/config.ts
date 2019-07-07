2707afc5954d52acd823673e7b5a0c45fc3a81b2
Jedrzej <progreacja@gmail.com>
2019-07-07 17:18:56 +0200

    Add Now setups

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,3 +1,4 @@
 export const endpoint = `http://localhost:4444/graphql`;
+export const prodEndpoint = `http://localhost:4444/graphql`;
 
 export const perPage = 10
--- frontend/utils/withApolloConfigured.ts
+++ frontend/utils/withApolloConfigured.ts
@@ -1,12 +1,12 @@
 import ApolloClient from 'apollo-boost'
 import withApollo from 'next-with-apollo'
 
-import { endpoint } from '../app/config'
+import { endpoint, prodEndpoint } from '../app/config'
 
 export const withApolloConfigured = withApollo(
   ({ headers }) =>
     new ApolloClient({
-      uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+      uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
       request: async operation => {
         await operation.setContext({
           fetchOptions: {
