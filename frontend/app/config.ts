b8dc401ba1045bee851522e879debfde3d5f4c6a
Jedrzej <progreacja@gmail.com>
2019-07-14 19:08:28 +0200

    update json -frontend

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,4 +1,4 @@
 export const endpoint = `http://localhost:4444/graphql`;
-export const prodEndpoint = `http://localhost:4444/graphql`;
+export const prodEndpoint = `https://blog-graphql-backend.herokuapp.com/graphql`;
 
 export const perPage = 10
